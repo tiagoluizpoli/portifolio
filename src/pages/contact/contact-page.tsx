a64b49b316616e6d756c0e4d1a8463154de311dd
Tiago Poli <tiagoluizpoli@gmail.com>
2025-01-13 00:01:10 -0300

    refactor: move form to a different file

--- src/pages/contact/contact-page.tsx
+++ src/pages/contact/contact-page.tsx
@@ -1,21 +1,6 @@
-import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { Textarea } from '@/components/ui/textarea';
-import { type Contact, contactSchema, useContactMutation, useSolutionsQuery } from '@/core';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { motion } from 'framer-motion';
-import { useForm } from 'react-hook-form';
 import { FaEnvelope, FaMapMarkerAlt, FaWhatsapp } from 'react-icons/fa';
+import { ContactForm } from './contact-form';
 
 const info = [
   {
@@ -36,34 +21,6 @@ const info = [
 ];
 
 export const ContactPage = () => {
-  const { data: solutions, isLoading, isFetching } = useSolutionsQuery();
-  const { mutateAsync } = useContactMutation();
-  const form = useForm({
-    resolver: zodResolver(contactSchema),
-    defaultValues: {
-      name: '',
-      phoneNumber: '',
-      email: '',
-      solution: solutions?.[0].title ?? '',
-      message: '',
-    },
-  });
-  const { control, handleSubmit, reset } = form;
-
-  const onSubmit = async (data: Contact) => {
-    console.log(data);
-    await mutateAsync(data);
-    reset();
-  };
-
-  if (isFetching || isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!solutions) {
-    return <div>No data</div>;
-  }
-
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -81,105 +38,7 @@ export const ContactPage = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
             {/* form */}
-            <Form {...form}>
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                <h3 className="text-4xl text-accent">Let's work together</h3>
-                <p className="text-white/60">
-                  Go ahead and send me a message, I'll get back to you as soon as possible.
-                </p>
-                {/* inputs */}
-                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                  <FormField
-                    control={control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input type="name" placeholder="Name" {...field} ref={null} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={control}
-                    name="phoneNumber"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input type="phone" placeholder="Phone number" {...field} ref={null} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <FormField
-                  control={control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input type="email" placeholder="Email address" {...field} ref={null} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* select */}
-
-                <FormField
-                  control={control}
-                  name="solution"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} value={field.value}>
-                        <FormControl>
-                          <SelectTrigger className="w-full">
-                            <SelectValue placeholder={'Select a solution'} />
-                          </SelectTrigger>
-                        </FormControl>
-
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Select an option below</SelectLabel>
-                            {solutions.map((item) => {
-                              return (
-                                <SelectItem key={item.id} value={item.title}>
-                                  {item.title}
-                                </SelectItem>
-                              );
-                            })}
-                            <SelectItem value="other">Other...</SelectItem>
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* textarea */}
-                <FormField
-                  control={control}
-                  name="message"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Textarea className="h-[200px]" placeholder="leave me a message here" {...field} ref={null} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {/* btn */}
-                <Button size={'md'} className="max-w-40" type="submit">
-                  Send message
-                </Button>
-              </form>
-            </Form>
+            <ContactForm />
           </div>
 
           {/* info */}
