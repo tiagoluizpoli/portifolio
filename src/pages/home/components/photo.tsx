b0e5feb9097bbf615960f4037b3682c56bbb1e9e
Tiago Poli <tiagoluizpoli@gmail.com>
2025-01-14 19:25:06 -0300

    refactor: added the dashed circling animation around the picture

--- src/pages/home/components/photo.tsx
+++ src/pages/home/components/photo.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const Photo = ({ picture }: Props) => {
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative flex justify-center items-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
@@ -20,7 +20,7 @@ export const Photo = ({ picture }: Props) => {
       >
         {/* image */}
         <motion.div
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-difference absolute border-[6px] border-accent rounded-full "
+          className="w-[300px] h-[300px] xl:w-[506px] xl:h-[506px] mix-blend-difference absolute p-3"
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
@@ -31,39 +31,44 @@ export const Photo = ({ picture }: Props) => {
             },
           }}
         >
-          <img src={picture} alt="" className="object-contain rounded-full" />
+          <div className="border-[6px] border-accent rounded-full">
+            <img src={picture} alt="" className="object-contain rounded-full" />
+          </div>
         </motion.div>
 
         {/* circle */}
 
         <motion.svg
           className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-          fill={'transparent'}
+          fill="transparent"
           viewBox="0 0 506 506"
-          xmlns={'http://www.w3.org/2000/svg'}
+          xmlns="http://www.w3.org/2000/svg"
         >
           <title>turning circle</title>
-          {/* <motion.circle
+          <motion.circle
             cx="253"
             cy="253"
-            r={'250'}
-            stroke={'var(--color-accent)'}
-            strokeWidth={'4'}
-            strokeLinecap={'round'}
-            strokeLinejoin={'round'}
+            r="250"
+            stroke="var(--color-accent)"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{
+              transformOrigin: '50% 50%',
+            }}
             initial={{
-              strokeDasharray: '24 10 0 0',
+              strokeDasharray: '15 128 25 25',
+              strokeDashoffset: 0,
             }}
             animate={{
-              strokeDasharray: ['15 128 25 25', '16 25 92 72', '4 250 22 22'],
-              rotate: [120, 360],
+              strokeDashoffset: [0, -1000], // Adjust the value to control the rotation speed
             }}
             transition={{
               duration: 20,
               repeat: Number.POSITIVE_INFINITY,
-              repeatType: 'reverse',
+              ease: 'linear',
             }}
-          /> */}
+          />
         </motion.svg>
       </motion.div>
     </div>
