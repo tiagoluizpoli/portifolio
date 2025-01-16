b271afaed67d653c390e424b355c70121d1d2494
Tiago Poli <tiagoluizpoli@gmail.com>
2025-01-16 00:43:25 -0300

    refactor: use name from cms in the header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,26 @@
+import { useHomeQuery } from '@/core';
 import { Link } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { MobileNav } from './mobile-nav';
 import { Nav } from './nav';
 
 export const Header = () => {
+  const { data, isLoading, isFetching } = useHomeQuery();
+  if (isLoading || isFetching) {
+    return <div className="flex justify-center">Loading...</div>;
+  }
+
+  if (!data) {
+    return <div className="flex justify-center">No Data</div>;
+  }
+
   return (
     <header className="py-8 text-white xl:py-12">
       <div className="container flex items-center justify-between mx-auto">
         <Link to={'/'}>
-          <h1 className="text-4xl font-semibold">
-            Tiago<span className="text-accent">.</span>
+          <h1 className="text-4xl font-semibold capitalize">
+            {data.firstName}
+            <span className="text-accent">.</span>
           </h1>
         </Link>
 
